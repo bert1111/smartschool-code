182e0649f46fce8bb3fd586bf1cbf7733ca265c9
bert1111 <130929618+bert1111@users.noreply.github.com>
2025-09-29 18:27:48 +0200

    Update objects.py
    Added a few colors. How many are there?

--- objects.py
+++ objects.py
@@ -36,7 +36,7 @@ class CourseGraphic:
 @dataclass
 class ResultGraphic:
     """Represents the graphical part of a result."""
-    color: Literal["green", "red", "yellow", "blue", "orange", "steel"]
+    color: Literal["green", "red", "yellow", "blue", "orange", "steel", "olive", "aqua"]
     symbol: Optional[str] = None  # Optioneel gemaakt
     description: Optional[str] = ""  # Toegevoegd optioneel attribuut
 
